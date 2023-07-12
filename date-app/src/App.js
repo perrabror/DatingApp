fac2e430793e80105f4de00eb711bbf91679b899
Petter <carlsson93@msn.com>
2023-07-12 19:19:07 +0200

    maxat

--- date-app/src/App.js
+++ date-app/src/App.js
@@ -4,13 +4,43 @@ import React, { useState } from 'react';
 import Card from './components/card/card.js';
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import bootstrapjs from 'bootstrap/dist/js/bootstrap.min.js';
+// import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+
 
 function App() {
   
   
   
   return (
-    <Card />
+    <div >
+    <header>
+    {/* <Router> */}
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container">
+    <h3 className="navbar-brand">My App</h3>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+          </li>
+      
+        </ul>
+      </div>
+    </div>
+  </nav>
+    {/* <Routes> */}
+      {/* <Route path="/" element={<Card />}/>                 */}
+     <Card />
+
+  
+
+  {/* </Routes>
+  </Router> */}
+
+    </header>
+  </div>
    
   );
 }
--- date-app/src/components/card/card.js
+++ date-app/src/components/card/card.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import ladyGnome from "../gnomes/lady.jpg";
 import gardenGnome from "../gnomes/garden.jpg";
 import laughGnome from "../gnomes/laugh.jpg";
@@ -10,35 +9,61 @@ import summerGnome from "../gnomes/summer.jpg";
 const allGnomes = [
     { file: gardenGnome, description: "garden gnome", price: 400 },
     { file: ladyGnome, description: "lady gnome", price: 120 },
-    { file: laughGnome, description: "laughing gnome", price: 300 },
-    { file: militaryGnome, description: "military gnome", price: 240 },
+    { file: laughGnome, description: "laughing gnome", price: 100 },
+    { file: militaryGnome, description: "military gnome", price: 80 },
     { file: redHatGnome, description: "red hat", price: 10 },
     { file: summerGnome, description: "double summer", price: 80 }
 ]
 
-function Card() {
+function Card(props) {
     const [currentGnomeIndex, setCurrentGnomeIndex] = useState(0);
-
-    const nextGnome = () => {
-        setCurrentGnomeIndex((prevIndex) => {
-            return (prevIndex + 1) % allGnomes.length;  // Wraps around to the first gnome after the last gnome
-        });
-    };
-
+    const [prospects, setProspects] = useState([]);
+    const [isYes, setIsYes] = useState(false);
     const gnome = allGnomes[currentGnomeIndex];
+    const [gnomePrice, setGnomePrice] = useState(Math.floor(Math.random() * 400) + 1);
+
+    const nextGnome = (isYes) => {
+        if (isYes && gnomePrice <= 100) {
+            setProspects((prevProspects) => [...prevProspects, gnome]);
+            // setIsYes(false);
+            console.log(prospects)
+        }
+        setCurrentGnomeIndex((prevIndex) => (prevIndex + 1) % allGnomes.length);
+         // Wraps around to the first gnome after the last gnome
+        };
+        useEffect(() => {
+            setGnomePrice(Math.floor(Math.random() * 400) + 1);
+        }, [currentGnomeIndex]);
+    
+
 
     return (
+        <div>
+        <div className="w-25 position-absolute">
+                <h3>Prospects</h3>
+                <ul className="list-group float-end">
+                    {prospects.map((prospect) => (
+                        <li className="list-group-item" key={prospect.description}>
+                            {prospect.description}
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
         <div className="d-flex flex-column mt-5 w-100 align-items-center border">
+            <h1>Do you like this gnome?</h1>
+            
             <div className="card p-3">
                 <h5>{gnome.description.toUpperCase()}</h5>
             <img src={gnome.file} className="d-block" style={{width: "200px", height: "200px"}} alt={gnome.description} />
             <div>
-                <p>{gnome.price} kr</p>
+                <p>{gnomePrice} kr</p>
             </div>
             <div className="d-flex flex-row justify-content-between">
-            <button className="btn btn-secondary"onClick={nextGnome}>No</button>
-            <button className="btn btn-secondary"onClick={nextGnome}>Yes</button>
+            <button className="btn btn-secondary"onClick={ () =>nextGnome(false)}>No</button>
+            <button className="btn btn-secondary"onClick={ () =>nextGnome(true)}>Yes</button>
 </div>  
+        </div>
         </div>
         </div>
     ); 

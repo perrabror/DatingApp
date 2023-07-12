30d3001afbf5802bf7728f5666e618224cc97f8d
Petter <carlsson93@msn.com>
2023-07-12 20:13:35 +0200

    match

--- date-app/src/components/card/card.js
+++ date-app/src/components/card/card.js
@@ -5,6 +5,7 @@ import laughGnome from "../gnomes/laugh.jpg";
 import militaryGnome from "../gnomes/military.jpg";
 import redHatGnome from "../gnomes/red-hat.jpg";
 import summerGnome from "../gnomes/summer.jpg";
+import match from "../gnomes/nedladdning.png";
 
 const allGnomes = [
     { file: gardenGnome, description: "garden gnome", price: 400 },
@@ -18,28 +19,31 @@ const allGnomes = [
 function Card(props) {
     const [currentGnomeIndex, setCurrentGnomeIndex] = useState(0);
     const [prospects, setProspects] = useState([]);
-    const [isYes, setIsYes] = useState(false);
+    const [lastMatchedGnome, setLastMatchedGnome] = useState(null);
     const gnome = allGnomes[currentGnomeIndex];
     const [gnomePrice, setGnomePrice] = useState(Math.floor(Math.random() * 400) + 1);
+    const [showMatch, setShowMatch] = useState(false);
 
     const nextGnome = (isYes) => {
-        if (isYes && gnomePrice <= 100) {
+        if (showMatch) {
+            setShowMatch(false);
+        }
+        else if (isYes && gnomePrice <= 200) {
             setProspects((prevProspects) => [...prevProspects, gnome]);
-            // setIsYes(false);
-            console.log(prospects)
+            setLastMatchedGnome(gnome);
+            setShowMatch(true);
         }
         setCurrentGnomeIndex((prevIndex) => (prevIndex + 1) % allGnomes.length);
-         // Wraps around to the first gnome after the last gnome
-        };
-        useEffect(() => {
-            setGnomePrice(Math.floor(Math.random() * 400) + 1);
-        }, [currentGnomeIndex]);
-    
+    };
 
+    useEffect(() => {
+        if(!showMatch) 
+        {setGnomePrice(Math.floor(Math.random() * 400) + 1);}
+    }, [currentGnomeIndex, showMatch]);
 
     return (
         <div>
-        <div className="w-25 position-absolute">
+            <div className="w-25 position-absolute">
                 <h3>Prospects</h3>
                 <ul className="list-group float-end">
                     {prospects.map((prospect) => (
@@ -48,23 +52,22 @@ function Card(props) {
                         </li>
                     ))}
                 </ul>
-
             </div>
-        <div className="d-flex flex-column mt-5 w-100 align-items-center border">
-            <h1>Do you like this gnome?</h1>
-            
-            <div className="card p-3">
-                <h5>{gnome.description.toUpperCase()}</h5>
-            <img src={gnome.file} className="d-block" style={{width: "200px", height: "200px"}} alt={gnome.description} />
-            <div>
-                <p>{gnomePrice} kr</p>
+            <div className="d-flex flex-column mt-5 w-100 align-items-center border">
+                <h1>{showMatch ? `You matched with ${lastMatchedGnome.description}` : "Do you like this gnome?"}</h1>
+                
+                <div className="card p-3">
+                    <h5>{showMatch ? "" : gnome.description.toUpperCase()}</h5>
+                    <img src={showMatch ? match : gnome.file} className="d-block" style={{width: "200px", height: "200px"}} alt={gnome.description} />
+                    <div>
+                        <p>{gnomePrice} kr</p>
+                    </div>
+                    <div className="d-flex flex-row justify-content-between">
+                        <button className="btn btn-secondary" onClick={() => nextGnome(false)}>No</button>
+                        <button className="btn btn-secondary" onClick={() => nextGnome(true)}>Yes</button>
+                    </div>  
+                </div>
             </div>
-            <div className="d-flex flex-row justify-content-between">
-            <button className="btn btn-secondary"onClick={ () =>nextGnome(false)}>No</button>
-            <button className="btn btn-secondary"onClick={ () =>nextGnome(true)}>Yes</button>
-</div>  
-        </div>
-        </div>
         </div>
     ); 
 }
